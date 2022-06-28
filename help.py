876134d718df78ccac817c89267569c8d67290c2
zlidzen <m.karsakou@gmail.com>
2022-06-28 14:08:49 +0300

    make link ttrabc -> help. Add param -h. Add param checker.

--- help.py
+++ help.py
@@ -1,6 +1,15 @@
-def main():
-  pass
+def printHelp():
+  print(""" There is app help!
+  """)
 
+def wrongParamsCount():
+  print("Wrong params count. Try ttrabc -h.")
+
+def unknownParam():
+  print("Unknown param. Try ttrabc -h.")
+
+def main():
+  printHelp
 
 if __name__ == "__main__":
   main()
--- ttrabc.py
+++ ttrabc.py
@@ -1,6 +1,20 @@
-def main():
-  pass
+import sys
+import help
 
+def main(arg1 = "-h", arg2=""):
+  if arg1 == "-h" or arg1 == "":
+    help.printHelp()
+  else:
+    help.unknownParam()
 
 if __name__ == "__main__":
-  main()
+  paramsCount = len(sys.argv)
+
+  if paramsCount == 2:
+     main(sys.argv[1])
+
+  elif paramsCount == 3:
+    main(sys.argv[1], sys.argv[2])
+  
+  else:
+    help.wrongParamsCount()
